f2058b00a65601e9a3e0085b45b1cd70d0fc78a4
Ahim11 <ahim.mudbari@gmail.com>
2022-06-27 17:05:05 +0545

    playing with node packages

--- npms/index.js
+++ npms/index.js
@@ -2,3 +2,10 @@ const _ = require('underscore');
 
 var result = _.contains([1, 2, 3], 2);
 console.log(result);
+var calc = require('ahimsha');
+let addition = calc.displayAdd(1, 1);
+console.log(addition);
+let p = 11,
+  q = 7;
+let subtraction = calc.displaySubtract(p, q);
+console.log(`subtraction of ${p} and ${q} is ${subtraction}`);
