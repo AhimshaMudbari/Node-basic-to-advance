9bbf0eab3278a76bca89fa194358f995a70bb6a3
Ahim11 <ahim.mudbari@gmail.com>
2022-07-09 22:22:38 +0545

    mongo schema and validation of genre

--- movie_Api/models/genre.js
+++ movie_Api/models/genre.js
@@ -1,19 +1,22 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
+const genreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+    minlength: 3,
+    maxlength: 15,
+  },
+});
+const Genre = mongoose.model('Genre', genreSchema);
 
-const Genre = mongoose.model(
-  'Genre',
-  mongoose.Schema({
-    name: { type: String, require: true, minlength: 3, maxlength: 15 },
-    date: { type: Date, default: Date.now },
-  })
-);
-
-validategenre = (g) => {
+function validategenre(g) {
   const schema = Joi.object({
     name: Joi.string().required(),
   });
   return schema.validate(g);
-};
+}
+
+exports.genreSchema = genreSchema;
 exports.Genre = Genre;
 exports.validate = validategenre;
