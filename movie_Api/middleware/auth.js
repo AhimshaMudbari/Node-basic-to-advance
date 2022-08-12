faad26360421635eec1190762faceced778f0419
Ahim11 <ahim.mudbari@gmail.com>
2022-08-12 10:56:07 +0545

    adding authentication middleware to valid jwt user

--- movie_Api/middleware/auth.js
+++ movie_Api/middleware/auth.js
@@ -1,4 +1,17 @@
-function auth(req, res, next) {
-  const token = req.header('X-auth-token');
-  if (!token) res.status(401).send('Access denied no token provided');
-}
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const jwtSecret = process.env.JWT;
+
+module.exports = function (req, res, next) {
+  const header = req.header('x-auth-token');
+  if (!header) return res.status(401).send('Access denied, no token provided');
+
+  try {
+    const validPayload = jwt.verify(header, jwtSecret);
+    req.user = validPayload;
+    next();
+  } catch {
+    res.status(400).send('Invalid token');
+  }
+};
