28a16c2bb22731440ab42eebf311a3a3fe6e49a9
Ahim11 <ahim.mudbari@gmail.com>
2022-08-22 22:19:15 +0545

    test of array, number, string, object

---
+++ testing/testing.test.js
@@ -0,0 +1,66 @@
+const { expect, describe } = require('@jest/globals');
+const { PosNeg, str, arr, obj } = require('./number');
+
+describe('positive-negative-zero', () => {
+  it('should return positive for positive', () => {
+    const result = PosNeg(1);
+    expect(result).toBe(1);
+  });
+
+  it('should return positive for negative', () => {
+    const result = PosNeg(-1);
+    expect(result).toBe(1);
+  });
+
+  it('should return zero for zero\n', () => {
+    const result = PosNeg(0);
+    expect(result).toBe(0);
+  });
+});
+
+describe('Test string', () => {
+  it('should return hello message\n', () => {
+    const result = str('Ahim mudbari');
+    // expect(result).toContain('Ahim');
+    expect(result).toMatch(/aHIM/i);
+  });
+});
+
+describe('Array test', () => {
+  it('should return array of movies\n', () => {
+    const result = arr();
+    // expect(result).not.toBeNull();
+    // expect(result).toContain('GOT', 'Rio', 'Harry potter');
+
+    // expect(result[0]).toContain('GOT');
+    // expect(result[1]).toBe('Rio');
+    // expect(result[2]).toMatch(/harry potter/i);
+    // expect(result.length).toBe(3);
+
+    expect(result).toEqual(
+      expect.arrayContaining(['GOT', 'Rio', 'Harry potter'])
+    );
+  });
+});
+
+describe('Oject Test', () => {
+  it('should return oject of GOT', () => {
+    const result = obj(1);
+
+    // expect(result).toEqual({
+    //   characterId: 1,
+    //   house: 'Stark',
+    //   character: 'jon snow',
+    //   age: 27,
+    // });
+
+    expect(result).toMatchObject({
+      id: 1,
+      house: 'Stark',
+      character: 'jon snow',
+      age: 27,
+    });
+
+    expect(result).toHaveProperty('id', 32);
+  });
+});
